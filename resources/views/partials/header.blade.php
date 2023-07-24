7881571bb377a86decf3b8886fdf15949711963d
GianmauroCoviello <gianmaurocoviello@gmail.com>
2023-07-24 04:35:21 +0200

    [DOCS] inserimento cartella img e file comics.php

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,11 @@
+<header>
+    <div class="container-fluid bg-primary">
+        <div class="row">
+            <div class="col-10 text-end text-white">
+                <span class="pe-5">ADDITIONAL DC</span>
+                <span >ADDITIONAL DC</span>      
+            </div>
+            
+        </div>
+    </div>
+</header>
