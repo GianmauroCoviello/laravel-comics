dc29d7b6f9cb4c10966af116abc30893d8f34c63
GianmauroCoviello <gianmaurocoviello@gmail.com>
2023-07-24 04:54:16 +0200

    impostazione layout header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -5,6 +5,19 @@
                 <span class="pe-5">ADDITIONAL DC</span>
                 <span >ADDITIONAL DC</span>      
             </div>
+
+            
+        </div>
+    </div>
+    <div class="container-fluid ">
+        <div class="row justify-content-center">
+            <div class="col-8 py-3 d-flex ">
+                <img src="{{Vite::asset('./resources/images/dc-logo.png')}}" alt="">
+                <ul>
+                    <li><a href="">CHARACTERS</a></li>
+                </ul>
+            </div>
+            
             
         </div>
     </div>
