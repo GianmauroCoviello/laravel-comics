4d6c10e69a19ffefb52f26e4f698548087c767f6
GianmauroCoviello <gianmaurocoviello@gmail.com>
2023-07-24 11:04:38 +0200

    fine strutturazione layout header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -13,10 +13,26 @@
         <div class="row justify-content-center">
             <div class="col-8 py-3 d-flex ">
                 <img src="{{Vite::asset('./resources/images/dc-logo.png')}}" alt="">
-                <ul>
-                    <li><a href="">CHARACTERS</a></li>
-                </ul>
+                <nav class="menu">
+                    <ul>
+                        <li>
+                            <a href="#">CHARACTERS</a>
+                            <a href="#">HOME</a>
+                            <a href="#">COMICS</a>
+                            <a href="#">MOVIES</a>
+                            <a href="#">TV</a>
+                            <a href="#">GAMES</a>
+                            <a href="#">COLLECTIBLES</a>
+                            <a href="#">VIDEOS</a>
+                            <a href="#">FANS</a>
+                            <a href="#">NEWS</a>
+                            <a href="#">SHOP</a>
+                        </li>
+                    </ul>
+                    <input type="text" placeholder="search">
+                </nav>                
             </div>
+            
             
             
         </div>
